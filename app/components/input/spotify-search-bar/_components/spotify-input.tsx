2aef321602fcdd9bd845009b422cbbebee22a3c9
jocelinqueau <queau.j@gmail.com>
2024-08-17 14:24:12 +0200

    ⚡️ fixing the spotify search bar

--- app/components/input/spotify-search-bar/_components/spotify-input.tsx
+++ app/components/input/spotify-search-bar/_components/spotify-input.tsx
@@ -5,6 +5,7 @@ import { XStack } from "joceui";
 import { useLayoutEffect, useRef } from "react";
 
 import { Input, InputProps } from "@/components/ui/input";
+import { cn } from "@/lib/classnames";
 
 interface _InputProps extends InputProps {
   right?: React.ReactNode;
@@ -23,21 +24,25 @@ const _Input = ({
 
   useLayoutEffect(() => {
     if (right || left) {
-      console.log("here");
       ref.current?.classList.add(
         "focus:outline-none",
         "focus:ring-0",
         "focus-visible:ring-0",
-        "border-0",
+        "border-none",
         "focus-visible:ring-offset-0",
         "bg-inherit",
       );
-      ref.current?.classList.remove("ring-offset-background");
+      ref.current?.classList.remove(
+        "ring-offset-background",
+        "focus-visible:ring-2",
+        "focus-visible:ring-ring",
+        "focus-visible:ring-offset-2"
+      );
     }
   }, [right, left]);
 
   return (
-    <XStack center gap={gap} className={className}>
+    <XStack center gap={gap} className={cn("focus-within:ring-2 focus-within:ring-border", className)}>
       {left && left}
       <Input ref={ref} {...props} />
       {right && right}
